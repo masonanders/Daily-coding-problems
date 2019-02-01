d30f2f6733ff3f78a1e9003925afb85942d4f057
MasonA <mason@ourprairie.net>
2019-01-31 18:01:53 -0800

    run bash command file through formatter

--- .bashrc-commands.bash
+++ .bashrc-commands.bash
@@ -3,22 +3,23 @@
 alias codeproblems='cd ~/Documents/projects/coding-problems/ && clear'
 
 function test() {
-  if [[ "$PWD" == "/Users/mason/Documents/projects/coding-problems/daily"
-    || "$PWD" == "/Users/mason/Documents/projects/coding-problems/javascript" ]]; then
+  if [[ "$PWD" == "/Users/mason/Documents/projects/coding-problems/daily" || "$PWD" == "/Users/mason/Documents/projects/coding-problems/javascript" ]]; then
     if [[ -z "$1" ]]; then
       echo "must supply a file name to test"
     elif [[ ! -f $1 && ! -f "$1.js" ]]; then
       echo "$1 could not be found"
     else
       node "test/$1"
-    fi;
+    fi
+  elif [[ "$PWD" == "/Users/mason/Documents/projects/coding-problems" ]]; then
+    echo "must navigate to \"./daily\" or \"./javascript\" to execute this command"
   else
     echo "must navigate to Documents/.../coding-problems to execute this command"
     echo "type 'codeproblems' to jump there now"
-  fi;
+  fi
 }
 
-function newproblem() { 
+function newproblem() {
   if [[ "$PWD" == "/Users/mason/Documents/projects/coding-problems/daily" || "$PWD" == "/Users/mason/Documents/projects/coding-problems/javascript" ]]; then
     if [[ -z $1 ]]; then
       echo "must supply a file name (function name optional)"
@@ -27,12 +28,14 @@ function newproblem() {
     else
       FILENAME=$1
       FUNCTIONNAME=${2:-$FILENAME}
-      echo -e "// Description here\n\nfunction $FUNCTIONNAME(/*arguments*/) {\n  // code here\n}\n\n// Time: O(?)\n// Space: O(?)\n\nmodule.exports = $FUNCTIONNAME;" >> "$FILENAME.js"
-      echo -e "const UnitTest = require(\"../../test/unit-test\");\nconst $FUNCTIONNAME = require(\"../$FILENAME\");\n\nconst test = new UnitTest($FUNCTIONNAME);\n// test.createTestCase({\n//   output: \"test\",\n//   args: [\"arg1\", \"arg2\"],\n//   description: \"this is an example\"\n// });\n\ntest.runTests();" >> "test/$FILENAME.js"
+      echo -e "// Description here\n\nfunction $FUNCTIONNAME(/*arguments*/) {\n  // code here\n}\n\n// Time: O(?)\n// Space: O(?)\n\nmodule.exports = $FUNCTIONNAME;" >>"$FILENAME.js"
+      echo -e "const UnitTest = require(\"../../test/unit-test\");\nconst $FUNCTIONNAME = require(\"../$FILENAME\");\n\nconst test = new UnitTest($FUNCTIONNAME);\n// test.createTestCase({\n//   output: \"test\",\n//   args: [\"arg1\", \"arg2\"],\n//   description: \"this is an example\"\n// });\n\ntest.runTests();" >>"test/$FILENAME.js"
       echo "created $FILENAME.js and test/$FILENAME.js"
-    fi;
+    fi
+  elif [[ "$PWD" == "/Users/mason/Documents/projects/coding-problems" ]]; then
+    echo "must navigate to \"./daily\" or \"./javascript\" to execute this command"
   else
     echo "must navigate to Documents/.../coding-problems to execute this command"
     echo "type 'codeproblems' to jump there now"
-  fi;
+  fi
 }
