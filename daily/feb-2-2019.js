14f4920541dffa4a2dabf3a832eb300043b4c6b7
MasonA <mason@ourprairie.net>
2019-02-03 14:28:05 -0800

    start feb 2 and fix test cases

--- daily/feb-2-2019.js
+++ daily/feb-2-2019.js
@@ -1,13 +1,32 @@
-// Given an array of integers, find the first missing positive integer in 
-// linear time and constant space. In other words, find the lowest positive 
-// integer that does not exist in the array. The array can contain duplicates 
+// Given an array of integers, find the first missing positive integer in
+// linear time and constant space. In other words, find the lowest positive
+// integer that does not exist in the array. The array can contain duplicates
 // and negative numbers as well.
 
+function firstMissingPosIntWithoutDups(array) {
+  let sumOfPositives = 0;
+  let maxInt = array[0];
+  for (let i in array) {
+    if (array[i] > 0) sumOfPositives += array[i];
+    if (array[i] > maxInt) maxInt = array[i];
+  }
+  return sumFromOneToN(maxInt) - sumOfPositives || 1;
+}
+
+function sumFromOneToN(n) {
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
 function firstMissingPosInt(array) {
-  // code here
+  let missingInt = 1;
+  let maxInt = array[0];
 }
 
 // Time: O(?)
 // Space: O(?)
 
-module.exports = firstMissingPosInt;
+module.exports = firstMissingPosIntWithoutDups;
--- daily/test/feb-2-2019.js
+++ daily/test/feb-2-2019.js
@@ -4,27 +4,27 @@ const firstMissingPosInt = require("../feb-2-2019");
 const test = new UnitTest(firstMissingPosInt);
 test.createTestCase({
   description: "finds the first positive integer in an array",
-  input: [3, 4, -1, 1],
+  input: [[3, 4, -1, 1]],
   output: 2
 });
 test.createTestCase({
   description: "can contain duplicates",
-  input: [1, 2, 2, 0],
+  input: [[1, 2, 2, 0]],
   output: 3
 });
 test.createTestCase({
   description: "can contain negatives",
-  input: [-2, 5, 3, 0, -1, 1, 2],
+  input: [[-2, 5, 3, 0, -1, 1, 2]],
   output: 4
 });
 test.createTestCase({
   description: "with all negative integers",
-  input: [-1],
+  input: [[-1]],
   output: 1
 });
 test.createTestCase({
   description: "with an empty array",
-  input: [],
+  input: [[]],
   output: 1
 });
 
