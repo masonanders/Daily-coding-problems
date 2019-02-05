d819cc9200b2ad598645396dab6170ef428b41f5
MasonA <mason@ourprairie.net>
2019-02-04 23:59:58 -0800

    finish how many steps

--- javascript/howManyStepCombos.js
+++ javascript/howManyStepCombos.js
@@ -1,7 +1,21 @@
 // Description here
 
-function howManyStepCombos(/*arguments*/) {
-  // code here
+function howManyStepCombos(arrOfSteps, numSteps) {
+  if (numSteps === 1) return [[1]];
+  const stepCombos = new Set();
+  arrOfSteps.forEach(step => {
+    if (step > numSteps) return;
+    if (step === numSteps) stepCombos.add(JSON.stringify([step]));
+    const prevStepCombos = howManyStepCombos(arrOfSteps, numSteps - step);
+    prevStepCombos.forEach(combo => {
+      for (let i = 0; i <= combo.length; i++) {
+        const comboCopy = combo.slice();
+        comboCopy.splice(i, 0, step);
+        stepCombos.add(JSON.stringify(comboCopy));
+      }
+    });
+  });
+  return Array.from(stepCombos).map(combo => JSON.parse(combo));
 }
 
 // Time: O(?)
