d4675d504f0179f40b74946a3908c98fd8e44b84
MasonA <mason@ourprairie.net>
2019-02-13 22:36:57 -0800

    finish problem

--- daily/feb-6-2019.js
+++ daily/feb-6-2019.js
@@ -1,10 +1,40 @@
-// Description here
+// A unival tree (which stands for "universal value") is a tree where
+// all nodes under it have the same value.
+// Given the root to a binary tree, count the number of unival subtrees.
 
-function univalTreeCount(/*arguments*/) {
-  // code here
+function univalTreeCount(root) {
+  let count = 0;
+  const recHelper = function(node, value) {
+    if (!node) return true;
+    if (value === undefined) {
+      const child = node.left || node.right;
+      if (child) value = child.value;
+    }
+    if (
+      (!node.left || node.left.value === value) &&
+      (!node.right || node.right.value === value)
+    ) {
+      if (recHelper(node.left, value) && recHelper(node.right, value))
+        count += 1;
+      return true;
+    } else {
+      const child = node.left || node.right;
+      value = child ? child.value : undefined;
+      if (
+        recHelper(node.left, value) &&
+        recHelper(node.right, value) &&
+        (!node.left || node.left.value === value) &&
+        (!node.right || node.right.value === value)
+      ) {
+        count += 1;
+      }
+    }
+    return false;
+  };
+  recHelper(root);
+  return count;
 }
-
-// Time: O(?)
-// Space: O(?)
+// Time: O(N)
+// Space: average (logN) | worst-case O(N)
 
 module.exports = univalTreeCount;
--- daily/test/feb-6-2019.js
+++ daily/test/feb-6-2019.js
@@ -1,11 +1,55 @@
 const UnitTest = require("../../test/unit-test");
 const univalTreeCount = require("../feb-6-2019");
 
+class Node {
+  constructor(value, left = null, right = null) {
+    this.value = value;
+    this.left = left;
+    this.right = right;
+  }
+}
+const uniTree1 = new Node(0);
+const uniTree5 = new Node(
+  0,
+  new Node(1),
+  new Node(0, new Node(1, new Node(1), new Node(1)), new Node(0))
+);
+const uniTree6 = new Node(
+  0,
+  new Node(1),
+  new Node(0, new Node(1, new Node(1), new Node(1)), new Node(1))
+);
+const uniTree8 = new Node(
+  0,
+  new Node(1),
+  new Node(
+    0,
+    new Node(1, new Node(1), new Node(1)),
+    new Node(1, new Node(0), new Node(0, new Node(0)))
+  )
+);
+
 const test = new UnitTest(univalTreeCount);
-// test.createTestCase({
-//   description: "this is an example",
-//   input: ["arg1", "arg2"],
-//   output: "test"
-// });
+test.createTestCase({
+  description: "should return number of unival trees",
+  input: [uniTree5],
+  output: 5
+});
+test.createTestCase({
+  description: "should count a unival trees children as well as itself",
+  input: [uniTree6],
+  output: 6
+});
+test.createTestCase({
+  description:
+    "should count a tree as unival even if the root value does not match the children",
+  input: [uniTree8],
+  output: 8
+});
+test.createTestCase({
+  description: "should return 1 if node has no children",
+  input: [uniTree1],
+  output: 1
+});
 
 test.runTests();
