68d9f2e8bb83d8030ad525f38d99a57e6762e32d
MasonA <mason@ourprairie.net>
2019-02-13 23:22:41 -0800

    prettier solution

--- daily/feb-6-2019.js
+++ daily/feb-6-2019.js
@@ -2,37 +2,60 @@
 // all nodes under it have the same value.
 // Given the root to a binary tree, count the number of unival subtrees.
 
+// function univalTreeCount(root) {
+//   let count = 0;
+//   const recHelper = function(node, value) {
+//     if (!node) return true;
+//     if (value === undefined) {
+//       const child = node.left || node.right;
+//       if (child) value = child.value;
+//     }
+//     if (
+//       (!node.left || node.left.value === value) &&
+//       (!node.right || node.right.value === value)
+//     ) {
+//       if (recHelper(node.left, value) && recHelper(node.right, value))
+//         count += 1;
+//       return true;
+//     } else {
+//       const child = node.left || node.right;
+//       value = child ? child.value : undefined;
+//       if (
+//         recHelper(node.left, value) &&
+//         recHelper(node.right, value) &&
+//         (!node.left || node.left.value === value) &&
+//         (!node.right || node.right.value === value)
+//       ) {
+//         count += 1;
+//       }
+//     }
+//     return false;
+//   };
+//   recHelper(root);
+//   return count;
+// }
+
 function univalTreeCount(root) {
-  let count = 0;
-  const recHelper = function(node, value) {
-    if (!node) return true;
-    if (value === undefined) {
-      const child = node.left || node.right;
-      if (child) value = child.value;
-    }
+  function recHelper(node) {
+    if (!node) return [0, true];
+
+    const [leftCount, leftIsUnival] = recHelper(node.left);
+    const [rightCount, rightIsUnival] = recHelper(node.right);
+    let [count, isUnival] = [0, false];
+    const child = node.left || node.right;
+
     if (
-      (!node.left || node.left.value === value) &&
-      (!node.right || node.right.value === value)
+      (!node.left || node.left.value === child.value) &&
+      (!node.right || node.right.value === child.value) &&
+      leftIsUnival &&
+      rightIsUnival
     ) {
-      if (recHelper(node.left, value) && recHelper(node.right, value))
-        count += 1;
-      return true;
-    } else {
-      const child = node.left || node.right;
-      value = child ? child.value : undefined;
-      if (
-        recHelper(node.left, value) &&
-        recHelper(node.right, value) &&
-        (!node.left || node.left.value === value) &&
-        (!node.right || node.right.value === value)
-      ) {
-        count += 1;
-      }
+      count += 1;
+      if (!child || node.value === child.value) isUnival = true;
     }
-    return false;
-  };
-  recHelper(root);
-  return count;
+    return [leftCount + rightCount + count, isUnival];
+  }
+  return recHelper(root)[0];
 }
 // Time: O(N)
 // Space: average (logN) | worst-case O(N)
